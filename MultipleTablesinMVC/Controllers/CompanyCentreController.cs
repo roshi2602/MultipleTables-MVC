71a2607a13dbfe19c2b8aa13b3fa6f53c0cebc5c
roshi2602 <roshidubey2602@gmail.com>
2021-07-13 17:06:07 +0530

    first commit

---
+++ MultipleTablesinMVC/Controllers/CompanyCentreController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultipleTablesinMVC.Models;
+namespace MultipleTablesinMVC.Controllers
+{
+    public class CompanyCentreController : Controller
+    {
+        // GET: CompanyCentre
+        public ActionResult Index()
+        {
+            //add db context
+            CompanyDBEntities3 cd3 = new CompanyDBEntities3();
+            //create list of company centres and add to db context object
+            List<CompanyCentre> cc = cd3.CompanyCentres.ToList();
+            return View(cc);
+            
+        }
+
+        public ActionResult Details(int companyid)  //passing parameter myid
+        {
+            //add db context
+            CompanyDBEntities3 cd3 = new CompanyDBEntities3();
+            //create companycentre objectn and add to add db context
+            CompanyCentre ce = cd3.CompanyCentres.SingleOrDefault(x => x.ccId == companyid);
+
+
+            return View(ce);
+        }
+    }
+}
---
+++ MultipleTablesinMVC/Controllers/CompanyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultipleTablesinMVC.Models;
+namespace MultipleTablesinMVC.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult MyCompany()
+        {
+            //add db context
+            CompanyDBEntities3 cd3 = new CompanyDBEntities3();
+            //create list of company centres and add to db context object
+            List<Company> cc = cd3.Companies.ToList();
+            return View(cc);
+        }
+    }
+}
